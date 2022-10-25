8837dd8da0ec11b27b37fc3fe7ad132a6ff40e70
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-25 15:03:50 +0200

    Add routing to movies view after login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -6,6 +6,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -18,7 +20,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    public router: Router) { }
 
   ngOnInit(): void {
   }
@@ -29,9 +32,10 @@ export class UserLoginFormComponent implements OnInit {
       localStorage.setItem('token', result.token);
       this.dialogRef.close(); //closes modal on success
       console.log(result),
-        this.snackBar.open('User logged in successfully', 'OK', {
-          duration: 2000
-        });
+        this.router.navigate(['movies']);
+      this.snackBar.open('User logged in successfully', 'OK', {
+        duration: 2000
+      });
     }, (result) => {
       console.log(result); // for testing
       this.snackBar.open(result, 'OK', {
