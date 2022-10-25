ac3a8d4871210bf2eb826a9a07c5232626a8a7ea
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-25 18:40:44 +0200

    Create synopsis view (dialog component)

---
+++ src/app/synopsis-view/synopsis-view.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-synopsis-view',
+  templateUrl: './synopsis-view.component.html',
+  styleUrls: ['./synopsis-view.component.css']
+})
+export class SynopsisViewComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA)
+  public data: {
+    Movie: { Title: String, Description: String }
+  }
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
