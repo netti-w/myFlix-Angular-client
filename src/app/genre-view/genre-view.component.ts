fa19770d23f6670b72b624236eb964d69fea4557
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-25 18:40:03 +0200

    Pass data to genre view

--- src/app/genre-view/genre-view.component.ts
+++ src/app/genre-view/genre-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-view',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./genre-view.component.css']
 })
 export class GenreViewComponent implements OnInit {
-
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA)
+  public data: {
+    Genre: { Name: String, Description: String }
+  }
+  ) { }
 
   ngOnInit(): void {
   }
