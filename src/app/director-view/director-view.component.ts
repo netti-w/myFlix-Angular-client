e409a096f7cd395ce25b208b9d356e71de127e4f
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-25 17:50:40 +0200

    Create director-view (dialog) component

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-view',
+  templateUrl: './director-view.component.html',
+  styleUrls: ['./director-view.component.css']
+})
+export class DirectorViewComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Director: { Name: String, Bio: String, Birth: Date, Death: Date }
+    }
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
