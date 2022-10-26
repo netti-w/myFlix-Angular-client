8ac4cc85621e5b1d2c7b323e31306f3f5ebeb203
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-26 12:10:34 +0200

    Add route path to profile view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: 'profile', component: ProfileViewComponent },
 ];
 
 @NgModule({
