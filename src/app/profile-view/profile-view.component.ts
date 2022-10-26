0834fc484d6ff44d92dafae245ac58a904e5369b
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-26 14:22:10 +0200

    Add showing profile detail data

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.css']
 })
 export class ProfileViewComponent implements OnInit {
+  user: any = {};
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.getUser();
+    // console.log(this.user); // for testing
   }
 
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = resp;
+      console.log(this.user);
+      return this.user;
+    });
+  }
+
+  openMovieView(): void {
+    this.router.navigate(['movies']);
+  }
 }
