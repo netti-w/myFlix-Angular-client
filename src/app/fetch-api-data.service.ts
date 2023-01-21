e346ed311520eb740b3334e75de61f9892199d96
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-21 20:36:35 +0100

    Update APIurl to Vercel

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://myflix-nw.herokuapp.com/';
+const apiUrl = 'https://vercel-test-netti-w.vercel.app/';
 
 @Injectable({
   providedIn: 'root'
